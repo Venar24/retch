eb6a234063067eafa1bbe7cad17c3d645401f708
Venar <venar.oathbreaker@outlook.com>
2024-12-16 15:48:58 -0500

    added cpu model, still needs some work

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,17 @@ fn main() {
     let total_memory_kb = total_memory_bt / 1024;
     let total_memory_mb = total_memory_kb / 1024;
     let total_memory_gb = total_memory_mb / 1024;
+    //get cpu average utils
+    if let Some(cpu) = system.cpus().first() {
+        let brand = cpu.brand();
+        let frequency_mhz = cpu.frequency(); // Frequency in MHz
+        println!("CPU Model: {} @ {:.2} GHz", brand, frequency_mhz as f64 / 1000.0);
+    }
     
     println!("OS: {}
-Ram: {} Gb", std::env::consts::OS,total_memory_gb);
+Ram: {} Gb",
+              
+              
+              std::env::consts::OS,
+              total_memory_gb);
 }
